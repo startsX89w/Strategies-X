78eba68e99a2db683b2df7d7a3d38ef7e2cf8a81
startsX89w <okr59a@gmail.com>
2025-11-01 13:24:48 +0100

    Update MainSource.lua

--- TDS/MainSource.lua
+++ TDS/MainSource.lua
@@ -13,7 +13,7 @@ local Items = {
 }
 
 local LoadLocal = false
-local MainLink = LoadLocal and "" or "https://raw.githubusercontent.com/startsX89w/Strategies-X/main/TDS/"
+local MainLink = LoadLocal and "" or "https://raw.githubusercontent.com/startsX89w/Strategies-X/refs/heads/main/TDS/"
 
 local OldTime = os.clock()
 
